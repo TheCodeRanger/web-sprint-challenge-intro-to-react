e08bcdcb57ecde07c410393cf640f748cd915785
Christopher Greene <greenebasecamp@gmail.com>
2021-02-07 20:02:22 -0500

    further progress, stuck at rending issues

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,5 +2,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Character = (props) => {
+    return(
+        <charStyle>
+            <div className="charCards">
+                <h2>Name: {props.character.name}</h2>
+            </div>
+        </charStyle>
+    )
+}
 
-
+export default Character;
